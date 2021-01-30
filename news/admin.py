7bea5609085b3e168e3cefdd2635660874760e83
Rizqi Maulana <sakukode@gmail.com>
2021-01-30 13:37:53 +0700

    Menambahkan modul Category pada Django Admin

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+# import class model Category
+from .models import Category
 
-# Register your models here.
+
+# Menambahkan modul Category ke Admin
+class CategoryAdmin(admin.ModelAdmin):
+    # Menampilkan kolom "name" dan "created_at" pada halaman Category list di Admin
+    list_display = ('name', 'created_at')
+    # Menambahkan fitur pencarian berdasarkan kolom "name"
+    search_fields = ['name']
+
+
+# Mendaftarkan CategoryAdmin
+admin.site.register(Category, CategoryAdmin)
