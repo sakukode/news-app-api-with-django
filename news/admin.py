f74391e1709f29f6a63344b7e8a8f406999eb4e3
Rizqi Maulana <sakukode@gmail.com>
2021-01-30 13:39:30 +0700

    Memodifikasi modul User pada Django Admin

--- news/admin.py
+++ news/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
+# import class yang dibutuhkan untuk memodifikasi form pada User Admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.models import User
 # import class model Category
 from .models import Category
 
@@ -10,6 +14,48 @@ class CategoryAdmin(admin.ModelAdmin):
     # Menambahkan fitur pencarian berdasarkan kolom "name"
     search_fields = ['name']
 
-
+    
 # Mendaftarkan CategoryAdmin
 admin.site.register(Category, CategoryAdmin)
+
+
+# Membuat Custom Form untuk Form Add User
+class UserCreateForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', )
+
+
+# Membuat Custom Form untuk Form Update User
+class UserUpdateForm(UserChangeForm):
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', )
+        
+        
+# Mengeset Custom User Form ke modul User Admin
+class UserAdmin(UserAdmin):
+    add_form = UserCreateForm
+    form = UserUpdateForm
+    prepopulated_fields = {'username': ('first_name', 'last_name', )}
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('first_name', 'last_name', 'email', 'username', 'password1', 'password2', ),
+        }),
+    )
+    
+    fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('first_name', 'last_name', 'email', 'username', ),
+        }),
+    )
+
+
+# Mendaftarkan ulang User Admin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
